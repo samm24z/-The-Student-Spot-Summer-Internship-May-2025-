1ae044b6f5b178e02adc34d59df4f8a2f629bff0
samm24z <abdussamad1682@gmail.com>
2025-06-14 19:18:17 +0530

    Update bfs.py

--- Task_1_BFS_DFS/bfs.py
+++ Task_1_BFS_DFS/bfs.py
@@ -1,1 +1,29 @@
+from collections import deque
 
+# Graph as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+def bfs(start_node):
+    visited = set()
+    queue = deque([start_node])
+    visited.add(start_node)
+
+    while queue:
+        node = queue.popleft()
+        print(node, end=' ') # Use end=' ' for space-separated output
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+# Run BFS
+print("Breadth-First Search starting from node A:")
+bfs('A')
+print() # Add a newline at the end for clean output
